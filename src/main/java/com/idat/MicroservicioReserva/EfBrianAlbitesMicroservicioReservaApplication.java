231ee0705b39636803de04326b8ca7a85ff0ca9a
brian albites <sl73212959@idat.edu.pe>
2022-12-13 20:35:13 -0500

    holaag

---
+++ src/main/java/com/idat/MicroservicioReserva/EfBrianAlbitesMicroservicioReservaApplication.java
@@ -0,0 +1,13 @@
+package com.idat.MicroservicioReserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfBrianAlbitesMicroservicioReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfBrianAlbitesMicroservicioReservaApplication.class, args);
+	}
+
+}
